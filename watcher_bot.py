45e3ffd774537ba076d9559f9d88a9e9d5ea7740
MarcoTz <marco.tzschentke@gmail.com>
2024-06-17 22:09:40 +0200

    some fixes

--- watcher_bot.py
+++ watcher_bot.py
@@ -20,16 +20,16 @@ class WatcherBot:
         self.application = ApplicationBuilder().token(self.api_key).build()
 
         self.cmd_actions = [
-                ('help',            'show help message',                            self.get_help),
-                ('possible_shows $people',  'get list of possible shows to watch (separate people names by commas)',          self.get_possible),
-                ('add_watcher $person;$show',     'add person watching show',                     self.add_watcher),
-                ('remove_watcher $person;$show',  'remove person watching show',                  self.remove_watcher),
-                ('update_show %show;%nr',  'update episode number for show',               self.update_show),
-                ('add_show',        'add new show',                                 self.add_show),
-                ('show_shows $person', 'show shows person is watching',self.show_shows)
+                ('help',            '',              'show help message',                            self.get_help),
+                ('possible_shows',  '$people',       'get list of possible shows to watch',          self.get_possible),
+                ('add_watcher',     '$person;$show', 'add person watching show',                     self.add_watcher),
+                ('remove_watcher',  '$person;$show', 'remove person watching show',                  self.remove_watcher),
+                ('update_show',     '$show;$nr',     'update episode number for show',               self.update_show),
+                ('add_show',        '%show',         'add new show',                                 self.add_show),
+                ('show_shows',      '%person',       'show shows person is watching',                self.show_shows)
             ]
 
-        for (cmd,_,action) in self.cmd_actions:
+        for (cmd,_,_,action) in self.cmd_actions:
             new_handler : CommandHandler = CommandHandler(cmd,action)
             self.application.add_handler(new_handler)
 
@@ -46,14 +46,16 @@ class WatcherBot:
         if update.effective_message is None or update.effective_message.text is None:
             return ''
         else:
-            return update.effective_message.text
+            msg : str = update.effective_message.text
+            msg_cmd : str = msg.split(' ')[0]
+            return msg.replace(msg_cmd,'')
 
     async def get_help(self,update,context) -> None:
 
-        help_template : str = '/%s - %s' 
+        help_template : str = '/%s %s - %s' 
         help_strs : list[str] = []
-        for (cmd,cmd_info,_) in self.cmd_actions:
-            help_strs.append(help_template % (cmd,cmd_info))
+        for (cmd,cmd_args,cmd_info,_) in self.cmd_actions:
+            help_strs.append(help_template % (cmd,cmd_args,cmd_info))
 
 
         help_message : str = 'Possible commands:\n ' + '\n '.join(help_strs) 
@@ -62,7 +64,13 @@ class WatcherBot:
     
     async def get_possible(self,update,context) -> None:
         watcher_str : str = self.get_message_text(update)
-        watchers : list[str] = watcher_str.split(', ')
+        if watcher_str == '':
+            ret_msg : str = 'Please provide at least one person to see shows'
+            await self.send_message(update,context,ret_msg)
+            return
+
+        watchers : list[str] = list(map(lambda x: x.strip(),watcher_str.split(', ')))
+
 
         people_list : list[Person] = [] 
         for watcher_name in watchers:
@@ -73,9 +81,13 @@ class WatcherBot:
         shows : list[Show] = self.show_manager.get_possible_shows(people_list)
 
         watcher_names : list[str] = list(map(lambda x: x.name,people_list))
-        show_names : list[str] = list(map(lambda x: x.name,shows))
+        if len(shows) == 0:
+            ret_msg : str = 'No show to watch with %s ' % (', '.join(watcher_names))
+            await self.send_message(update,context,ret_msg)
+            return
 
-        ret_msg : str = 'Possible shows to watch with %s:\n\t %s' % (', '.join(watcher_names),', '.join(show_names))
+        show_strs : list[str] = list(map(lambda x: '%s (%s)' %(x.name,str(x.current_ep)),shows))
+        ret_msg : str = 'Possible shows to watch with %s:\n%s' % (', '.join(watcher_names),'\n'.join(show_strs))
         await self.send_message(update,context,ret_msg)
 
     async def add_watcher(self,update,context) -> None:
@@ -91,6 +103,7 @@ class WatcherBot:
 
         self.show_manager.add_watcher(new_watcher,new_show)
         ret_msg : str = 'Added %s to show %s' % (new_watcher,new_show)
+        self.show_manager.save_to_csv()
         await self.send_message(update,context,ret_msg)
     
     async def remove_watcher(self,update,context) -> None:
@@ -104,6 +117,7 @@ class WatcherBot:
         new_watcher : str = msg_args[0].strip()
         new_show : str = msg_args[1].strip()
         self.show_manager.remove_watcher(new_watcher,new_show)
+        self.show_manager.save_to_csv()
         ret_msg : str = 'Removed %s from %s' % (new_watcher,new_show)
         await self.send_message(update,context,ret_msg)
 
@@ -132,6 +146,7 @@ class WatcherBot:
             return
         
         show.update_ep(ep_nr)
+        self.show_manager.save_to_csv()
         ret_msg:str = 'Updated show %s' %show_name
         await self.send_message(update,context,ret_msg)
 
@@ -139,6 +154,7 @@ class WatcherBot:
     async def add_show(self,update,context) -> None:
         show_name : str = self.get_message_text(update).strip()
         self.show_manager.get_show_add(show_name)
+        self.show_manager.save_to_csv()
         ret_msg : str = 'Added show %s' % show_name
         await self.send_message(update,context,ret_msg)
 

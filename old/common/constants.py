6d25dcee33aec94ae199973f3b78a1ae36d5a011
MarcoTz <marco.tzschentke@gmail.com>
2024-09-22 20:45:38 +0200

    removed old code

--- old/common/constants.py
+++
@@ -1,4 +0,0 @@
-data_dir            : str = 'data'
-people_json         : str = 'people.json'
-shows_json          : str = 'shows.json'
-watchgroups_json    : str = 'watchgroups.json'
--- old/main.py
+++
@@ -1,7 +0,0 @@
-from common.ShowManager import ShowManager
-from watcher_bot import WatcherBot
-
-if __name__ == '__main__':
-    manager : ShowManager = ShowManager()
-    bot = WatcherBot(manager)
-    bot.run()
